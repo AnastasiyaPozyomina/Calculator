c69917a41c487442869c2e7609882c62840e4d8f
AnastasiyaPozyomina <pozyomina@gmail.com>
2020-05-02 03:41:02 +0700

    switch case

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-1. Переменные 
+1. Переменные https://learn.javascript.ru/variables
+2. Поиск:querySelector https://learn.javascript.ru/searching-elements-dom
+3. Откладка в браузере Chrome. Console.log https://learn.javascript.ru/debugging-chrome
+4. Введения события (обработчики событий,делегирование событий)https://learn.javascript.ru/events
+5. Атрибуты и DOM-свойства https://learn.javascript.ru/attributes-and-custom-properties
+6. Конструкция "switch" https://learn.javascript.ru/switch
+7. 
--- js.js
+++ js.js
@@ -1,3 +1,47 @@
+let currentValue = '';
 let inputCalc = document.querySelector('.calc-field');
+// console.log('inputCalc:', inputCalc);
+let inputBtn = document.querySelector('.calc-buttons');
+// console.log('inputBtn:', inputBtn);
 
-console.log('inputCalc:', inputCalc);
+
+function clickHandler(event) {
+    if (event.target && event.target.dataset && event.target.dataset.value) {
+        
+        switch (event.target.dataset.value) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case ',': setInputValue(event.target.dataset.value); break;
+            case 'C': clearInputValue(); break;
+            case '+': plusValues(event.target.dataset.value); break;
+            case '-': minusValues(event.target.dataset.value); break;
+            case '*':multiplyValues(event.target.dataset.value); break;
+            case '÷':devidedValues(event.target.dataset.value); break;
+            case '=':resultValue(event.target.dataset.value); break;
+        }
+    }
+    
+}
+
+function setInputValue (value) {
+    currentValue += value;
+    inputCalc.value = currentValue;
+    // console.log('currentValue:', currentValue);
+    // console.log('value:', value);
+    // console.log(' inputCalc.value:',  inputCalc.value);
+}
+
+function clearInputValue() {
+    currentValue = '';
+    inputCalc.value = currentValue;
+}
+
+
